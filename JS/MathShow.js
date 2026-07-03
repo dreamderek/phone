523c7617e1790958d83c851e8431b971a7016a48
dreamderek <dreamderek@gmail.com>
2026-07-03 16:58:19 +0800

    01

---
+++ JS/MathShow.js
@@ -0,0 +1,59 @@
+window.MathJax = {
+    tex: { inlineMath: [['\\(', '\\)']], displayMath: [['\\[', '\\]']] }
+};
+
+if (!document.getElementById('mathjax-lib')) {
+    const s = document.createElement('script');
+    s.id = 'mathjax-lib';
+    s.async = true;
+    s.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+    document.head.appendChild(s);
+}
+
+class MathShow extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" }).innerHTML = `
+         <style>
+                :host {
+                    margin: 0;
+                    padding: 0;
+                    width: 100%;
+                    height: 100%;
+                    box-sizing: border-box;
+                    
+                    font-family: "PMingLiU", "標楷體", serif;
+                    font-size: 11pt;
+                    line-height: 1.2;
+                }
+                #main {
+                    width: 100%;
+                    height: 100%;
+                    padding: 10px;
+                    background-color: white;
+                    box-sizing: border-box;
+
+                    
+                }
+        </style>
+        
+        <div spellcheck="false" id="main">
+            <slot></slot>
+        </div>
+        `;
+
+        this._div = this.shadowRoot.querySelector('#main');
+    }
+
+    connectedCallback() {
+        this._typesetOne(this._div);
+    }
+
+    async _typesetOne(el) {
+        const MJ = window.MathJax;
+        if (!MJ) return;
+        if (MJ.startup?.promise) await MJ.startup.promise;
+        await MJ.typesetPromise([el]);
+    }
+
+} customElements.define("Math-Show", MathShow);
